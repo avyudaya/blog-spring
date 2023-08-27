c91aec53bf40cb85fca37e6dfda223178cea42a0
avyudaya <avyudaya7@gmail.com>
2023-08-27 12:56:19 +0545

    commit for review

--- src/main/java/com/avyudaya/blogservice/BlogServiceApplication.java
+++ src/main/java/com/avyudaya/blogservice/BlogServiceApplication.java
@@ -2,8 +2,10 @@ package com.avyudaya.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BlogServiceApplication {
 
 	public static void main(String[] args) {
